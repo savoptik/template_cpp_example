cc29efb74ade4bfdc5c02c1147ad286b7d512fd1
savoptik <sav70792@gmail.com>
2019-08-11 13:07:16 +0300

    Initial Commit

---
+++ template_cpp_example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  template_cpp_example
+//
+//  Created by Артём Семёнов on 11.08.2019.
+//  Copyright © 2019 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
